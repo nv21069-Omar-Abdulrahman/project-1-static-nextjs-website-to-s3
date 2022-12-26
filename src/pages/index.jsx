0c45ca45a751814c7032f39b9fb189f8b9b2e2b9
nv21069-Omar-Abdulrahman <NV21069@nvtc.edu.bh>
2022-12-26 10:24:58 +0300

    Update index.jsx
    name 2

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -263,10 +263,8 @@ export default function Home({ articles }) {
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
 Software Engineer student from 11 CCP          </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Omar, a software Engineer and student in NVTC Bahrain .Our instructor is Mr.Abdulrahman Khalid , we work on a lot of projects an labs .
+            
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
